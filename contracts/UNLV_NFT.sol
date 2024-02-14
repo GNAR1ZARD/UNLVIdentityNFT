947018346610d456687797826e067e65ee8f1358
GNAR1ZARD <Ryanamendala@gmail.com>
2024-02-14 14:42:44 -0800

    updated NFT contract functionality to verify based on signature and ID

--- contracts/UNLV_NFT.sol
+++ contracts/UNLV_NFT.sol
@@ -10,10 +10,10 @@ contract UNLVIdentityNFT is ERC721 {
     Counters.Counter private _tokenIds;
 
     // Mapping from student ID to their NFT token ID
-    mapping(uint => uint256) private _tokenByStudentId;
+    mapping(uint256 => uint256) private _tokenByStudentId;
 
     // Mapping to keep track of which student IDs have been used to mint an NFT
-    mapping(uint => bool) private _studentIdUsed;
+    mapping(uint256 => bool) private _studentIdUsed;
 
     // Mapping to keep track of which addresses have minted an NFT
     mapping(address => bool) private _addressMinted;
@@ -21,15 +21,21 @@ contract UNLVIdentityNFT is ERC721 {
     constructor() ERC721("UNLVIdentityNFT", "UNLV") {}
 
     // Function to mint new NFTs, restricted to one per student ID and one per Ethereum address
-    function mintNFT(uint studentID) public {
-        require(!_studentIdUsed[studentID], "This student ID has already minted an NFT.");
-        require(!_addressMinted[msg.sender], "This address has already minted an NFT.");
+    function mintNFT(uint256 studentID) public {
+        require(
+            !_studentIdUsed[studentID],
+            "This student ID has already minted an NFT."
+        );
+        require(
+            !_addressMinted[msg.sender],
+            "This address has already minted an NFT."
+        );
 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
-        
+
         _mint(msg.sender, newItemId);
-        
+
         // Mark the student ID and address as having minted an NFT
         _studentIdUsed[studentID] = true;
         _addressMinted[msg.sender] = true;
@@ -39,7 +45,7 @@ contract UNLVIdentityNFT is ERC721 {
     }
 
     // Function to check if a student ID has been used to mint an NFT
-    function isStudentIdUsed(uint studentID) public view returns (bool) {
+    function isStudentIdUsed(uint256 studentID) public view returns (bool) {
         return _studentIdUsed[studentID];
     }
 
@@ -49,8 +55,73 @@ contract UNLVIdentityNFT is ERC721 {
     }
 
     // Function to get the token ID associated with a student ID
-    function getTokenIdByStudentId(uint studentID) public view returns (uint256) {
-        require(_studentIdUsed[studentID], "This student ID has not been used to mint an NFT.");
-        return _tokenByStudentId[studentID];
+    // Requires a signature for verification
+    function getTokenIdByStudentId(uint256 studentID, bytes memory signature)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            _studentIdUsed[studentID],
+            "This student ID has not been used to mint an NFT."
+        );
+        uint256 tokenId = _tokenByStudentId[studentID];
+
+        // This is the message that the user signed
+        // It should be the same message that was signed on the client side
+        bytes32 message = keccak256(
+            abi.encodePacked("Verify student ID", studentID)
+        );
+        // This is the hash that was signed
+        // Message in the Ethereum signed message format
+        bytes32 ethSignedMessageHash = keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", message)
+        );
+
+        // Recover the address that signed the message
+        // Calls recoverSigner
+        address signer = recoverSigner(ethSignedMessageHash, signature);
+
+        // Verify that the signer is the owner of the NFT
+        require(
+            ownerOf(tokenId) == signer,
+            "Signature invalid or signer does not own the token"
+        );
+
+        return tokenId;
+    }
+
+    // Function to recover the signer address from a signature
+    function recoverSigner(bytes32 ethSignedMessageHash, bytes memory signature)
+        internal
+        pure
+        returns (address)
+    {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
+        return ecrecover(ethSignedMessageHash, v, r, s);
+    }
+
+    // Helper function to split a signature into its components
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "Invalid signature length");
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
     }
 }
